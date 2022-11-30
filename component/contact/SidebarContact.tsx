25ea161d9e683800d2804531e7ca69df85cbdbe6
trongpham <phamvantrong7.qn@gmail.com>
2022-11-30 20:54:44 +0700

    implement contact sidebar

--- component/contact/SidebarContact.tsx
+++ component/contact/SidebarContact.tsx
@@ -2,21 +2,81 @@ import Image from "next/image";
 import styles from "./SidebarContact.module.css";
 export default function SidebarContact() {
   return (
-    <div id="zalo-vr" className={styles.button_contact}>
-      <div className={styles.phone_vr}>
-        <div className={styles.phone_vr_circle_fill}></div>
+    <div className={styles.button_contact}>
+      <div className={styles.contact_vr}>
+        <div
+          className={`${styles.zalo_vr_circle_fill} ${styles.vr_circle_fill_first}`}
+        ></div>
+        <div
+          className={`${styles.zalo_vr_circle_fill} ${styles.vr_circle_fill_second}`}
+        ></div>
+        <div
+          className={`${styles.zalo_vr_circle_fill} ${styles.vr_circle_fill_third}`}
+        ></div>
+        <div className={styles.zalo_vr_img_circle}>
+          <a
+            className={styles.link}
+            target="_blank"
+            href="https://zalo.me/0333399053"
+          >
+            <Image
+              className={styles.image}
+              src="/zalo.png"
+              width={25}
+              height={27}
+              alt="ZALO MAZDA BÌNH DƯƠNG"
+            />
+          </a>
+        </div>
+      </div>
+      <div className={styles.contact_vr}>
+        <div
+          className={`${styles.phone_vr_circle_fill} ${styles.vr_circle_fill_first}`}
+        ></div>
+        <div
+          className={`${styles.phone_vr_circle_fill} ${styles.vr_circle_fill_second}`}
+        ></div>
+        <div
+          className={`${styles.phone_vr_circle_fill} ${styles.vr_circle_fill_third}`}
+        ></div>
         <div className={styles.phone_vr_img_circle}>
           <a
             className={styles.link}
             target="_blank"
-            href="https://zalo.me/0979358912"
+            href="https://zalo.me/0333399053"
           >
             <Image
-              className={styles.zalo_image}
-              src="https://mazdabinhduongauto.com/wp-content/plugins/button-contact-vr/img/zalo.png"
-              width={50}
-              height={50}
-              alt="dfdsf"
+              className={styles.image}
+              src="/phone.png"
+              width={25}
+              height={27}
+              alt="SDT MAZDA BÌNH DƯƠNG"
+            />
+          </a>
+        </div>
+      </div>
+      <div className={styles.contact_vr}>
+        <div
+          className={`${styles.fb_vr_circle_fill} ${styles.vr_circle_fill_first}`}
+        ></div>
+        <div
+          className={`${styles.fb_vr_circle_fill} ${styles.vr_circle_fill_second}`}
+        ></div>
+        <div
+          className={`${styles.fb_vr_circle_fill} ${styles.vr_circle_fill_third}`}
+        ></div>
+        <div className={styles.fb_vr_img_circle}>
+          <a
+            className={styles.link}
+            target="_blank"
+            href="https://www.facebook.com/profile.php?id=100069266504823"
+          >
+            <Image
+              className={styles.image}
+              src="/fb.jpg"
+              width={25}
+              height={27}
+              alt="FB MAZDA BÌNH DƯƠNG"
             />
           </a>
         </div>

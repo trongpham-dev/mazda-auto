3c41845eebbbdca2a85aeaa51faba477e7d886ac
trongpv <trongpv@unitech.vn>
2022-12-01 18:15:22 +0900

    fix slide

--- data/slider.tsx
+++ data/slider.tsx
@@ -7,23 +7,11 @@ export default interface SliderImage {
 
 export let sliderImages: SliderImage[] = [
   {
-    imgUrl: "/slide1.jpg",
-    alt: "MAZDA CX-5 2022",
+    imgUrl: "/slide/mazda-sport.jpg",
+    alt: "MAZDA SPORT 2022",
   },
   {
-    imgUrl: "/slide1.jpg",
-    alt: "MAZDA CX-30 2022",
+    imgUrl: "/slide/mazda-suv.jpg",
+    alt: "MAZDA SUV 2022",
   },
-  // {
-  //   imgUrl: "/slide/mazda-2.jpg",
-  //   alt: "MAZDA 2 2022",
-  // },
-  // {
-  //   imgUrl: "/slide/mazda-3.jpg",
-  //   alt: "MAZDA 3 2022",
-  // },
-  // {
-  //   imgUrl: "/slide/mazda-6.jpg",
-  //   alt: "MAZDA 6 2022",
-  // },
 ];

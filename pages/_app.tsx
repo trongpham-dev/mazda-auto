e9707eb0c992d0fa726c61494e56e46bc8d7a0f6
trongpham <phamvantrong7.qn@gmail.com>
2022-12-07 20:20:14 +0700

    setting gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../component/Layout/Layout";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Google Tag Manager
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-PCHGNDD" });
+  }, []);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
